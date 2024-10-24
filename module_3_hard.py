1bcae9efa485aeece7bf5bf4bacc6aac91e01df4
RetrixGYL <rugrns.stas@mail.ru>
2024-10-24 23:56:35 +0400

    Add files via upload

---
+++ module_3_hard.py
@@ -0,0 +1,21 @@
+def calculate_structure_sum(*data_structure):
+    sum_ = 0
+    for i in data_structure:
+        if isinstance(i, int):
+            sum_ += i
+        elif isinstance(i, str) and len(i) > 0 :
+            sum_ += len(i)
+        elif isinstance(i, (list, tuple, set)):
+            sum_ += calculate_structure_sum(*i)
+        elif isinstance(i, dict):
+            sum_ += calculate_structure_sum(*i.keys())
+            sum_ += calculate_structure_sum(*i.values())
+
+    return sum_
+
+
+
+
+
+results = calculate_structure_sum([[1, 2, 3],{'a': 4, 'b': 5},(6, {'cube': 7, 'drum': 8}),"Hello",((), [{(2, 'Urban', ('Urban2', 35))}])])
+print(results)
